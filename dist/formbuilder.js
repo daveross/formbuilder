2f942633836f19f138567009e09ad8db8d32f1a2
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2015-02-24 14:44:53 -0600

    $ is not defined, add it's alias as jQuery

--- dist/formbuilder.js
+++ dist/formbuilder.js
@@ -41,6 +41,7 @@
 }).call(this);
 
 (function() {
+  var $ = jQuery;
   var BuilderView, EditFieldView, Formbuilder, FormbuilderCollection, FormbuilderModel, ViewFieldView, _ref, _ref1, _ref2, _ref3, _ref4,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
